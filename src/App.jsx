70ccf883a332e990a30182ba73b448b1d801566e
Mateus Bessa <mateusbessacontato@gmail.com>
2023-11-17 18:23:01 -0300

    Alternando os estagios do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import {wordsList} from "./data/words.jsx"
 //stages
 const stages = [
   {id: 1, name: "start"},
-  {id: 2, name: "gamer"},
+  {id: 2, name: "game"},
   {id: 3, name: "end"},
 ]
 
@@ -23,14 +23,28 @@ function App() {
   const [count, setCount] = useState(0)
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
-  console.log(words)
+
+//start the secret-word game
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+//process the letter input
+const verifyLetter = () => {
+  setGameStage(stages[2].name)
+}
+
+//restarts the game
+const retry = () => {
+  setGameStage(stages[0].name)
+}
 
   return (
     <>
       <div id='Container'>
-        {gameStage === 'start' && <StartScreen />}
-        {gameStage === 'game' && <Game />}
-        {gameStage === 'end' && <GameOver />}
+        {gameStage === 'start' && <StartScreen startGame={startGame} />}
+        {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+        {gameStage === 'end' && <GameOver retry={retry} />}
       </div>
     </>
   )
