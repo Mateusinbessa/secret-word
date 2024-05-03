1f2d278783d3defff762740f49cbd0cb7c5b4753
Mateus Bessa <130805657+Mateusinbessa@users.noreply.github.com>
2024-05-03 20:31:07 -0300

    Update words.jsx

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -1,9 +1,19 @@
 export const wordsList = {
-    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-    programação: ["Linguagem", "Framework", "JavaScript", "React"],
-    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
-  };
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  animal: ["Cachorro", "Gato", "Elefante", "Leão", "Pássaro", "Tigre", "Cavalo", "Girafa", "Coelho", "Macaco"],
+  cor: ["Vermelho", "Azul", "Verde", "Amarelo", "Preto", "Branco", "Roxo", "Laranja", "Marrom", "Rosa"],
+  profissao: ["Médico", "Engenheiro", "Professor", "Advogado", "Padeiro", "Dentista", "Arquiteto", "Policial", "Bombeiro", "Enfermeiro"],
+  esporte: ["Futebol", "Basquete", "Natação", "Tênis", "Vôlei", "Atletismo", "Handebol", "Ginástica", "Surfe", "Boxe"],
+  pais: ["Estados Unidos", "Brasil", "China", "França", "Japão", "Índia", "Rússia", "Canadá", "Alemanha", "Reino Unido"],
+  instrumentoMusical: ["Violino", "Guitarra", "Piano", "Bateria", "Flauta", "Saxofone", "Trompete", "Violoncelo", "Clarinete", "Harp"],
+  filme: ["Titanic", "O Senhor dos Anéis", "Star Wars", "Harry Potter", "Jurassic Park", "Matrix", "Avatar", "Vingadores", "Crepúsculo", "Piratas do Caribe"],
+  objeto: ["Cadeira", "Mesa", "Caneta", "Livro", "Celular", "Chave", "Óculos", "Relógio", "Copo", "Computador"],
+  planta: ["Rosa", "Girassol", "Orquídea", "Samambaia", "Cacto", "Bonsai", "Margarida", "Lírio", "Violeta", "Jasmim"]
+}
+
 
